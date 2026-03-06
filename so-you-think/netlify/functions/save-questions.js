188e0adf1a859ed3d20e95764f3da5eef564dff3
TaylormacbrownC5 <taylormacbrown1@gmail.com>
2026-03-06 00:08:43 -0800

    Create save-questions.js

---
+++ so-you-think/netlify/functions/save-questions.js
@@ -0,0 +1,16 @@
+const { getStore } = require('@netlify/blobs');
+
+exports.handler = async (event) => {
+  const headers = { 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': 'Content-Type' };
+
+  if (event.httpMethod === 'OPTIONS') return { statusCode: 200, headers, body: '' };
+
+  try {
+    const { sessionId, questions } = JSON.parse(event.body);
+    const store = getStore('sessions');
+    await store.set(sessionId, JSON.stringify(questions));
+    return { statusCode: 200, headers, body: JSON.stringify({ ok: true }) };
+  } catch (err) {
+    return { statusCode: 500, headers, body: JSON.stringify({ error: err.message }) };
+  }
+};
